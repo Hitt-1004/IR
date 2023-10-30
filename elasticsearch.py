cc59a854565f0d75889e4064a39b3c4d17bf1799
Hitt-1004 <114856805+Hitt-1004@users.noreply.github.com>
2023-10-30 22:27:19 +0530

    Update elasticsearch.py

--- elasticsearch.py
+++ elasticsearch.py
@@ -204,3 +204,31 @@ total_precision = sum([sum(precision_results[query]) for query in queries])
 total_recall = sum([sum(recall_results[query]) for query in queries])
 print("Overall Average Precision:", total_precision / len(queries))
 print("Overall Average Recall:", total_recall / len(queries))
+
+
+#heat map
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+# Load the 20 Newsgroups dataset
+newsgroups = fetch_20newsgroups(subset='all', remove=('headers', 'footers', 'quotes'))
+
+# get random 20 documents
+documents = np.random.choice(newsgroups.data, size=30, replace=False)
+
+# Preprocess the documents (TF-IDF vectorization)
+tfidf_vectorizer = TfidfVectorizer()
+tfidf_matrix = tfidf_vectorizer.fit_transform(documents)
+
+# Calculate document similarity (cosine similarity)
+similarity_matrix = cosine_similarity(tfidf_matrix, tfidf_matrix)
+
+# Create a heatmap
+plt.figure(figsize=(20, 16))
+sns.heatmap(similarity_matrix, annot=True, cmap="YlGnBu", xticklabels=range(1, 31), yticklabels=range(1, 31))
+plt.title("Document Similarity Heatmap (First 30 Documents)")
+plt.show()
